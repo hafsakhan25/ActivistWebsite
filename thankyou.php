5e759416462918595ef3604863fb322aee1e9ca0
plum-22 <67593154+plum-22@users.noreply.github.com>
2020-07-29 17:56:41 +0100

    form changes

--- thankyou.php
+++ thankyou.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html>
+
 <html>
 <body>
 
-Thank You <?php echo $_POST["fname"]; ?><br>
+Thank_You <?php echo $_POST["fname"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
